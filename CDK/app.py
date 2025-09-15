4d81fd07288ad1c23c0be5f8366b6206b11d9efc
Leonardo Montesinos <leonardo.montesinos@utec.edu.pe>
2025-09-14 23:29:44 -0500

    Update app.py

--- CDK/app.py
+++ CDK/app.py
@@ -5,7 +5,7 @@ from crud_stack import CrudStack
 app = cdk.App()
 CrudStack(app, "CrudStack",
     env=cdk.Environment(
-        account="153265898954",  # tu cuenta AWS
+        account="310034235193",  # tu cuenta AWS
         region="us-east-1"
     )
 )
